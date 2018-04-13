b740734ee81fc1698714f40d1efddc2482be3e4b
João Silva <j.pedro004@gmail.com>
2018-04-13 23:35:33 +0100

    Fix request

--- compiler/server.py
+++ compiler/server.py
@@ -1,9 +1,27 @@
-from bottle import post, run
+from parser import Parser
 
-from main import Parser
+from bottle import hook, route, request, response, post, run
 
-@post("/compile")
+_allow_origin = '*'
+_allow_methods = 'PUT, GET, POST, DELETE, OPTIONS'
+_allow_headers = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
+
+@hook('after_request')
+def enable_cors():
+    '''Add headers to enable CORS'''
+
+    response.headers['Access-Control-Allow-Origin'] = _allow_origin
+    response.headers['Access-Control-Allow-Methods'] = _allow_methods
+    response.headers['Access-Control-Allow-Headers'] = _allow_headers
+
+@route('/', method = 'OPTIONS')
+@route('/<path:path>', method = 'OPTIONS')
+def options_handler(path = None):
+    return
+
+@post('/compile')
 def compile():
-    return "Hello, world!"
+    print(request.json)
+    return { "mate": "hello, world" }
 
-run(host="localhost", port=8080, debug=True)
+run()
