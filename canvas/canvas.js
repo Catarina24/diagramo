d6fa38fff61e739f4723c0ed93f7afecb866dd9c
Margarida Viterbo <margaridaviterbo@hotmail.com>
2018-04-13 19:42:01 +0100

    fixed display images

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -38,48 +38,169 @@ var offset;
 const size = 100;
 
 const object = {
-  "name" : "main.dpm",
-  "classes" : [
-      {
-          "name" : "database",
-          "position" : {"x":0, "y":0},
-          "image" : "http://www.prologicwebdesign.com/wp-content/uploads/2015/07/Oracle-Database-support.png",
-          "label" : "This is a label!"
-      },
-
-      {
-          "name" : "actor",
-          "position" : {"x":10, "y":10},
-          "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Robustness_Diagram_Actor.svg/1024px-Robustness_Diagram_Actor.svg.png"
-      }
-  ],
-  "objects" : [
-      {
-          "name" : "d1",
-          "parent" : "database"
-      },
-      {
-          "name" : "a1",
-          "parent" : "actor"
-      }
-  ]
-}
+  "classes": [
+        {
+            "connects": [
+                "adf"
+            ],
+            "image": {
+                "path": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Robustness_Diagram_Actor.svg/1024px-Robustness_Diagram_Actor.svg.png"
+            },
+            "label": "I am an actor mate",
+            "name": "actor",
+            "position": {
+                "x": 10,
+                "y": 20
+            }
+        },
+        {
+            "connects": [
+                "adf"
+            ],
+            "image": {
+                "path": "http://www.prologicwebdesign.com/wp-content/uploads/2015/07/Oracle-Database-support.png"
+            },
+            "label": "I am an db mate",
+            "name": "database",
+            "position": {
+                "x": 10,
+                "y": 20
+            }
+        },
+        {
+            "connects": [
+                "adf"
+            ],
+            "image": {
+                "path": "https://cdn0.iconfinder.com/data/icons/cloud-data-technology-1-3/63/47-512.png"
+            },
+            "label": "I am an server mate",
+            "name": "server",
+            "position": {
+                "x": 10,
+                "y": 20
+            }
+        },
+        {
+            "connects": [
+                "adf"
+            ],
+            "image": {
+                "path": "path/to/image.png"
+            },
+            "label": "I am an machine mate",
+            "name": "machine",
+            "position": {
+                "x": 10,
+                "y": 20
+            }
+        },
+        {
+            "connects": [],
+            "image": {
+                "path": "image/of/a/database.png"
+            },
+            "label": "Database",
+            "name": "system",
+            "position": {
+                "x": 0,
+                "y": 0
+            }
+        }
+    ],
+    "objects": [
+        {
+            "connects": [
+                "d1"
+            ],
+            "label": "I am so smart",
+            "name": "a1",
+            "parent": "actor",
+            "position": {
+                "x": 100,
+                "y": 100
+            }
+        },
+        {
+            "connects": [],
+            "label": "I am so smart",
+            "name": "d1",
+            "parent": "database",
+            "position": {
+                "x": 200,
+                "y": 100
+            }
+        },
+        {
+            "connects": [
+                "d1"
+            ],
+            "label": "I am so smart",
+            "name": "s1",
+            "parent": "server",
+            "position": {
+                "x": 200,
+                "y": 200
+            }
+        },
+        {
+            "connects": [
+                "s1"
+            ],
+            "image": {
+                "path": "https://www.shareicon.net/download/2015/12/26/693394_monitor.svg"
+            },
+            "label": "I am so smart",
+            "name": "m1",
+            "parent": "machine",
+            "position": {
+                "x": 200,
+                "y": 100
+            }
+        }
+    ]
+  };
 
 function preload() {
   //funcao
   object.objects.forEach(obj => {
       object.classes.forEach(objClass => {
           if(objClass.name == obj.parent){
-              var img = createImg(objClass.image);  // Load the image
-              img.hide();
-              loadedImages.push([objClass.position.x, objClass.position.y, img]);
+            var img;
+            if(obj.image == null){
+              img = createImg(objClass.image.path);  // Load the image
+            }
+            else{
+              img = createImg(obj.image.path);  // Load the image
+            }
+            img.hide();
+            var x;
+            var y;
+            if(obj.position == null){
+              x = objClass.position.x;
+              y = objClass.position.y;
+            }
+            else{
+              if(obj.position.x == null){
+                x = objClass.position.x;
+              }
+              else{
+                x = obj.position.x;
+              }
+              if(obj.position.y == null){
+                y = objClass.position.y;
+              }
+              else{
+                y = obj.position.y;
+              }
+            }
+            loadedImages.push([x, y, img]);
           }
       });
   });
 }
 
 function setup() {
-  console.log("fds");
   createCanvas(windowWidth, windowHeight);
   background(100);
   //parseElementsToDraw();
@@ -93,7 +214,6 @@ function placeImages() {
     images.push(new MyImage(pos, loadedImages[i][2].width, loadedImages[i][2].height, loadedImages[i][2]));
   }
 
-  console.log(images);
 }
 
 function draw() {
