dc11a0df73ed1e78da610bbbb809f0bfb50151e9
Catarina24 <catarina_juveleo@hotmail.com>
2018-04-14 21:24:26 +0100

    uploads folder .zip

--- canvas/js/project.js
+++ canvas/js/project.js
@@ -1,23 +1,45 @@
-var button = document.getElementById('export');
+var buttonExport = document.getElementById('export');
 
-button.addEventListener('click', function(event) {
+buttonExport.addEventListener('click', function (event) {
 
     var editors = $('.editor');
     var numberOfEditors = editors.length;
 
     var zip = new JSZip();
-    
+
     //Add files to folder "src"
-    var i=0;
-    for (var i = 0; i < editors.length; i++) {
-        var content = ace.edit(editors[i]).getValue();
-        zip.folder("src").file("file" + i + ".txt", content);
-    }
+
+    $("#my-editors-tabs").children(".editor-tab").each(function () {
+        let editor = this.dataset.number;
+        if (!editor) return;
+
+        var data = {};
+        data.name = $(this).find(".tab-name").first().text() + ".dgm"
+        data.content = ace.edit(editors[editor - 1]).getValue();
+
+        zip.folder("src").file(data.name, data.content);
+    });
 
     //Adds images to "resources"
 
-    //generates zip
-    zip.generateAsync({type:"blob"}).then(function(content) {
+    //Generates zip
+    zip.generateAsync({ type: "blob" }).then(function (content) {
         saveAs(content, "project.zip");
+    });
+});
+
+
+$('#folder-upload-form').on("submit", function (event) {
+    event.preventDefault();
+
+    $.ajax({
+        url: 'actions/upload-folder.php',
+        type: 'POST',
+        data: new FormData(this),
+        contentType: false, // NEEDED, DON'T OMIT THIS (requires jQuery 1.6+)
+        processData: false, // NEEDED, DON'T OMIT THIS
+        success: function (msg) {
+            console.log(msg);
+        }
     });
 });
